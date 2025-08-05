75c180c4b55b7458a56464a43e757c72766e1d2f
Kongphob <kongphobkong852@hotmail.com>
2025-08-05 17:09:28 +0700

    add auth

---
+++ backend/lib/redis.js
@@ -0,0 +1,12 @@
+import { Redis } from "@upstash/redis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+await redis.set("foo", "bar");
+await redis.get("foo");
