741c049b7db1cb38f8821fd66914c5cb83a33f4c
Kongphob <kongphobkong852@hotmail.com>
2025-08-06 17:19:10 +0700

    cart routes updated

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/auth/products", productRoutes);
+app.use("/api/auth/cart", cartRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port http://localhost:" + PORT);
